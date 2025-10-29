2cda19613173a5e136fd828c28759389baee0aa9
guohao.lu <1670212878@qq.com>
2025-10-29 16:00:29 +0800

    feat(database): 初始化财务发票配置规则数据
    - 添加Co-oP报表相关配置规则
    - 添加VC财务账单各类费用配置规则- 包含协议费用、广告费用、罚款、退货等分类规则
    - 设置优先级、财务科目、收支类型等字段信息- 配置创建和更新用户信息及时间戳

---
+++ oceanlink-common/src/test/java/org/example/SelfTest.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author guohao.lu
+ */
+public class SelfTest {
+    @Test
+    void test() {
+        System.out.println("hello world");
+    }
+}
