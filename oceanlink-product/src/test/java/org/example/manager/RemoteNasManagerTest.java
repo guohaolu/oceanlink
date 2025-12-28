9837c511d692019efb986146dd77166d4d27e453
guohao.lu <1670212878@qq.com>
2025-12-28 13:36:50 +0800

    test(RemoteNasManager): 添加删除功能测试用例
    - 为RemoteNasManager添加delete测试方法
    - 测试批量删除多个路径文件的功能
    - 包含多个欧洲地区文件的删除测试场景

--- oceanlink-product/src/test/java/org/example/manager/RemoteNasManagerTest.java
+++ oceanlink-product/src/test/java/org/example/manager/RemoteNasManagerTest.java
@@ -17,4 +17,23 @@ class RemoteNasManagerTest {
         List<String> list = remoteNasManager.list("/IT");
         System.out.println(list);
     }
+
+    @Test
+    void delete() {
+        List<String> paths = List.of(
+                "/IT/财务/影刀/平台/Amazon-VC/NetPPM/2025/VelwayVC-欧洲/2025-12-23/Net_PPM_ASIN_VelwayVC-欧洲_ALL_2024-01_\u200C_2025-12-23.xlsx",
+                "/IT/财务/影刀/平台/Amazon-VC/NetPPM/2025/VelwayVC-欧洲/2025-12-23/Net_PPM_ASIN_VelwayVC-欧洲_FR_2024-01_\u200C_2025-12-23.xlsx",
+                "/IT/财务/影刀/平台/Amazon-VC/NetPPM/2025/VelwayVC-欧洲/2025-12-23/Net_PPM_ASIN_VelwayVC-欧洲_PL_2024-01_\u200C_2025-12-23.xlsx",
+                "/IT/财务/影刀/平台/Amazon-VC/NetPPM/2025/VelwayVC-欧洲/2025-12-23/Net_PPM_ASIN_VelwayVC-欧洲_GB_2024-01_\u200C_2025-12-23.xlsx",
+                "/IT/财务/影刀/平台/Amazon-VC/NetPPM/2025/VelwayVC-欧洲/2025-12-23/Net_PPM_ASIN_VelwayVC-欧洲_DE_2024-01_\u200C_2025-12-23.xlsx",
+                "/IT/财务/影刀/平台/Amazon-VC/NetPPM/2025/VelwayVC-欧洲/2025-12-23/Net_PPM_ASIN_VelwayVC-欧洲_ES_2024-01_\u200C_2025-12-23.xlsx",
+                "/IT/财务/影刀/平台/Amazon-VC/NetPPM/2025/VelwayVC-欧洲/2025-12-23/Net_PPM_ASIN_VelwayVC-欧洲_SE_2024-01_\u200C_2025-12-23.xlsx",
+                "/IT/财务/影刀/平台/Amazon-VC/NetPPM/2025/VelwayVC-欧洲/2025-12-23/Net_PPM_ASIN_VelwayVC-欧洲_IT_2024-01_\u200C_2025-12-23.xlsx",
+                "/IT/财务/影刀/平台/Amazon-VC/NetPPM/2025/VelwayVC-欧洲/2025-12-23/Net_PPM_ASIN_VelwayVC-欧洲_NL_2024-01_\u200C_2025-12-23.xlsx"
+        );
+
+        for (String path : paths) {
+            remoteNasManager.delete(path);
+        }
+    }
 }
